70910ad12855b500d119a74c2f33c3897773c4e3
Muhammad Faisal <digital.1faisal@gmail.com>
2025-11-20 17:29:37 +0500

    update: class-30

--- js/30/script.js
+++ js/30/script.js
@@ -18,17 +18,94 @@
 //     console.log("Not a valid age - Please provide valid age")
 // }
 
-let color = prompt("Enter your color");
+// let color = prompt("Enter your color");
 
-if (color == "red") {
-    console.log("color is red")
-} else if (color == "blue") {
-    console.log("color is blue")
-} else if (color == "green") {
-    console.log("color is green")
-} else if (color == "yellow") {
-    console.log("color is yellow")
-} else {
-    console.log("There is no Color exist")
-}
+// if (color == "red") {
+//     console.log("color is red")
+// } else if (color == "blue") {
+//     console.log("color is blue")
+// } else if (color == "green") {
+//     console.log("color is green")
+// } else if (color == "yellow") {
+//     console.log("color is yellow")
+// } else {
+//     console.log("There is no Color exist")
+// }
 
+
+// {
+//     if (color == "red") {
+//         console.log("color is red")
+//     }
+//     console.log("going to next if 2")
+//     if (color == "blue") {
+//         console.log("color is blue")
+//     }
+
+//     console.log("going to next if 3")
+//     if (color == "green") {
+//         console.log("color is green")
+//     }
+
+//     console.log("going to next if 4")
+//     if (color == "yellow") {
+//         console.log("color is yellow")
+//     }
+// }
+
+// console.log("Game is start")
+
+// let unique = Math.ceil(Math.random() * 100);
+// console.log('\n')
+// console.log('\n')
+// console.log('\n')
+// console.log('\n')
+// console.log(unique)
+
+// let a = prompt("Enter your number");
+// if (a == unique) {
+//     console.log("Game Win")
+// } else {
+//     console.log("Game Lose")
+// }
+
+// console.log("Game end")
+
+// let color = prompt("Enter your color").toLowerCase();
+
+// console.log("Program start")
+
+// switch (color) {
+//     case 'red':
+//         console.log("color is red")
+//         break;
+//     case 'blue':
+//         console.log("color is blue")
+//         break;
+//     case 'green':
+//         console.log("color is green")
+//         break;
+//     case 'yellow':
+//         console.log("color is yellow")
+//         break;
+//     case 'black':
+//         console.log("color is black")
+//         break;
+//     default:
+//         console.log("We have no color of this type")
+// }
+
+// console.log("Program end")
+
+// let a = true ? "Condition is true" : "Condition is false";
+// console.log(a)
+
+// let a;
+
+// if (true) {
+//     a = "Condition is true";
+// } else {
+//     a = "Condition is false";
+// }
+
+// console.log(a)
